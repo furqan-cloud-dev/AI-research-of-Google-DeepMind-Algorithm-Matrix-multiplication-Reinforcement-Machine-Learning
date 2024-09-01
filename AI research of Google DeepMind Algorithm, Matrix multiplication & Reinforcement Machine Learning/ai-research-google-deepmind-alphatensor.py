7be0d3a11011c65cafb8bcfe1796b017e760bcc6
Furqan <macbook@Furqans-MacBook-Pro.local>
2024-09-01 23:05:51 +0500

    AI research of Google DeepMind Algorithm, Matrix multiplication & Reinforcement Machine Learning

---
+++ AI research of Google DeepMind Algorithm, Matrix multiplication & Reinforcement Machine Learning/ai-research-google-deepmind-alphatensor.py
@@ -0,0 +1,13 @@
+
+# Amazon Book Learning
+print("AI/ML Research Of Algorithms")
+
+infoDictionary = {"info": "Amazon Paperback Book"}
+infoDictionary["title"] = "Efficiency behind Google’s DeepMind Algorithm: 'AlphaTensor' for neural networks: AI research for discovering the fastest matrix multiplication with Reinforcement Machine Learning"
+infoDictionary["description"] = "Algorithms have helped mathematicians perform fundamental operations for thousands of years. The ancient Egyptians created an algorithm to multiply two numbers without requiring a multiplication table. Despite the familiarity with algorithms today – used throughout society from classroom algebra to cutting edge scientific research – the process of discovering new algorithms is incredibly difficult, and an example of the amazing reasoning abilities of the human mind"
+infoDictionary["description"] = "Anyone interested in AI/ML. Willing to know more about mathematics behind Deep Neural Networks, Achievements of DeepMind as a an AI Startup of the future innovation and about the first artificial intelligence (AI) system for discovering efficient and correct algorithms for fundamental tasks such as matrix multiplication.  This book is for you"
+infoDictionary["link"] = "https://www.amazon.com/dp/B0DF5NV2XQ"
+infoDictionary["language"] = "english"
+
+print(infoDictionary)
+
